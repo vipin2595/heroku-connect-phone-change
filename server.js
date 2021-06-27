481cf9c74f297ee852156f8758eb6bda78c9b2d4
Vipin <vipink2559@gmail.com>
2021-06-27 00:05:48 -0500

    test2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 app.set('port', process.env.PORT || 5000);
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 
 app.post('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
