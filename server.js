96e0fb465c12a417dcf749257526b3b04ae5ff51
Vipin <vipink2559@gmail.com>
2021-11-12 12:02:26 -0600

    adding a new post

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static('public'));
 app.use(bodyParser.json()); 
 
+app.post('/update2', function(req, res) {
+    console.log('req', req);
+});
+
 app.post('/update', function(req, res) {
     pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
         // watch for any connect issues
